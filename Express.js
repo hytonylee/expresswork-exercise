e6f3907f418818606f40f79a06df33d96b2fdf26
Tony Lee <hy.tony.lee@gmail.com>
2019-10-12 14:53:42 -0700

    Complete exercise #6

--- Express.js
+++ Express.js
@@ -1,22 +1,34 @@
-//// Expressworks Exercise #5: Stylish CSS
+//// Expressworks Exercise #6: Param Pam Pam
 const express = require('express')
 const app = express()
-app.use(require('stylus').middleware(process.argv[3]))
-app.use(express.static(process.argv[3]))
+app.put('/message/:id', (req, res) => {
+    const id = req.params.id
+    str = require('crypto')
+        .createHash('sha1')
+        .update(new Date().toDateString() + id)
+        .digest('hex')
+    res.send(str)
+})
 
 app.listen(process.argv[2])
+
+
+//// Expressworks Exercise #5: Stylish CSS
+// const express = require('express')
+// const app = express()
+// app.use(require('stylus').middleware(process.argv[3]))
+// app.use(express.static(process.argv[3]))
+// app.listen(process.argv[2])
 
 
 //// Expressworks Exercise #4: Good Old Form
 // const express = require('express')
 // const bodyParser = require('body-parser')
 // const app = express();
-
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.post('/form', (req, res) => {
 //     res.send(req.body.str.split('').reverse().join(''))
 // })
-
 // app.listen(process.argv[2])
 
 
