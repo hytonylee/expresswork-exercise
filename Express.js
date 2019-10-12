85a3ee431e5a95a4e7da6dd5a042449138d1d7d3
Tony Lee <hy.tony.lee@gmail.com>
2019-10-12 13:59:18 -0700

    Complete exercise 3

--- Express.js
+++ Express.js
@@ -1,4 +1,23 @@
-///
+/// Expressworks Exercise #3
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug')
+app.set('views', process.argv[3])
+app.get('/home', (req, res) => {
+    res.render('index', { date: new Date().toDateString() })
+})
+app.listen(process.argv[2])
+
+
+
+// const express = require('express')
+// const app = express()
+// app.set('view engine', 'pug')
+// app.set('views', process.argv[3])
+// app.get('/home', function (req, res) {
+//     res.render('index', { date: new Date().toDateString() })
+// })
+// app.listen(process.argv[2])
 
 
 //// Expressworks Exercise #2
@@ -15,4 +34,3 @@
 //     res.end('Hello World!')
 // })
 // app.listen(process.argv[2])
-
