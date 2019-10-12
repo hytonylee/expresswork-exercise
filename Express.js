20ac0b4f6d06b77f40e02ba22fc7695007725d47
Tony Lee <hy.tony.lee@gmail.com>
2019-10-12 14:22:15 -0700

    Complete Exercise 4

--- Express.js
+++ Express.js
@@ -1,33 +1,35 @@
-/// Expressworks Exercise #3
-const express = require('express');
+//// Expressworks Exercise #4: Good Old Form
+const express = require('express')
+const bodyParser = require('body-parser')
 const app = express();
-app.set('view engine', 'pug')
-app.set('views', process.argv[3])
-app.get('/home', (req, res) => {
-    res.render('index', { date: new Date().toDateString() })
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.post('/form', (req, res) => {
+    res.send(req.body.str.split('').reverse().join(''))
 })
+
 app.listen(process.argv[2])
 
 
-
-// const express = require('express')
-// const app = express()
+//// Expressworks Exercise #3: Pug
+// const express = require('express');
+// const app = express();
 // app.set('view engine', 'pug')
 // app.set('views', process.argv[3])
-// app.get('/home', function (req, res) {
+// app.get('/home', (req, res) => {
 //     res.render('index', { date: new Date().toDateString() })
 // })
 // app.listen(process.argv[2])
 
 
-//// Expressworks Exercise #2
+//// Expressworks Exercise #2: Static
 // const express = require('express');
 // const app = express();
 // app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
 // app.listen(process.argv[2])
 
 
-//// Expressworks Exercise #1
+//// Expressworks Exercise #1: Hello World!
 // const express = require('express');
 // const app = express();
 // app.get('/home', (req, res) => {
