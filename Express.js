37e8e1303c7170cc8d0e484f9e49527c82f1cda6
Tony Lee <hy.tony.lee@gmail.com>
2019-10-12 13:21:53 -0700

    Complete 1st and 2nd exercise

---
+++ Express.js
@@ -0,0 +1,18 @@
+///
+
+
+//// Expressworks Exercise #2
+// const express = require('express');
+// const app = express();
+// app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+// app.listen(process.argv[2])
+
+
+//// Expressworks Exercise #1
+// const express = require('express');
+// const app = express();
+// app.get('/home', (req, res) => {
+//     res.end('Hello World!')
+// })
+// app.listen(process.argv[2])
+
